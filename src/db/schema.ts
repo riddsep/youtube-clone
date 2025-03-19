7c5fdf8bc7bac2ec7750068282f5b77b7cc5c383
riddsep <ridoseptiawan00@gmail.com>
2025-03-19 18:06:40 +0700

    feat: add clerk auth and setup ngrok for webhook

---
+++ src/db/schema.ts
@@ -0,0 +1,19 @@
+import {
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+} from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable(
+  "users",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    clerkId: text("clerk_id").notNull(),
+    imageUrl: text("image_url").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow(),
+  },
+  (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]
+);
